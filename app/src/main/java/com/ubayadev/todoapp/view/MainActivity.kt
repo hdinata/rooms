87fee7de527cd139aa50396f9cddf09e2bfa26f6
Dosen-4A <tc@gmail.com>
2026-05-11 08:22:00 +0700

    nav

--- app/src/main/java/com/ubayadev/todoapp/view/MainActivity.kt
+++ app/src/main/java/com/ubayadev/todoapp/view/MainActivity.kt
@@ -2,14 +2,21 @@ package com.ubayadev.todoapp.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.ubayadev.todoapp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
     }
 }
